823f478a201b5cbbe41d1930e9c894f49fff91d1
MGoupy <132365940+MGoupy@users.noreply.github.com>
2023-05-02 16:33:25 +0200

    Update Analyse.py

--- Analyse.py
+++ Analyse.py
@@ -1,2 +1,33 @@
-print("test")
-print("Maxime est bizarre")
+import pandas as pd
+
+#%%
+##############################################################################
+### LOAD DATA ###
+##############################################################################
+
+dataset = pd.read_csv('dataset_classification_hojbo.csv')
+
+#%%
+##############################################################################
+### DATA CLEANING ###
+##############################################################################
+
+dataset_analyse=dataset.copy()
+
+#%%
+##############################################################################
+### DATA TREATMENT ###
+##############################################################################
+
+#Split data for each appartment appartement 1 is the test appartment, appartments 2-4 are the training ones
+dataset_test = dataset_analyse[(dataset_analyse['apt_no'] == 1)]
+
+dataset_training_1 = dataset_analyse[(dataset_analyse['apt_no'] == 2)]
+dataset_training_2 = dataset_analyse[(dataset_analyse['apt_no'] == 3)]
+dataset_training_3 = dataset_analyse[(dataset_analyse['apt_no'] == 4)]
+dataset_training_4 = dataset_analyse[(dataset_analyse['apt_no'] == 5)]
+
+#%%
+##############################################################################
+### TRAINING / TEST DATA ###
+##############################################################################
